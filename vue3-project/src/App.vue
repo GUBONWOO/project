90dc1c6ac89a9a22453e4a3c9bdc5e845d14d9b3
GUBONWOO <zcwxzsx@gmail.com>
2022-12-21 18:42:25 +0900

    vue - Todolist

---
+++ vue3-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="name">{{ name }}</div>
+  <button class="btn btn-primary" @click="update">Clcik</button>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const name = ref(' Koo coder')
+
+    // const greeting = (name) => {
+    //   return ' hello,' + name
+    // }
+    const update = () => {
+      name.value = 'ko ssie '
+      console.log(name)
+    }
+    return {
+      name,
+      update
+    }
+  }
+}
+</script>
+
+<style>
+.name {
+  color: red;
+}
+</style>
