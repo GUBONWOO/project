60ee76c0df91c187ca310d2c034779822a789eaf
GUBONWOO <zcwxzsx@gmail.com>
2023-02-15 17:23:55 +0900

    add:Pagination

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -17,6 +17,42 @@
       @toggle-todo="toggleTodo"
       @delete-todo="deleteTodo"
     />
+    <hr />
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li v-if="currentPage !== 1" class="page-item">
+          <a
+            style="cursor: pointer"
+            class="page-link"
+            @click="getTodos(currentPage - 1)"
+          >
+            Previous
+          </a>
+        </li>
+
+        <li
+          v-for="page in numberOfPages"
+          :key="page"
+          class="page-item"
+          :class="currentPage === page ? 'active' : ''"
+        >
+          <a
+            style="cursor: pointer"
+            class="page-link"
+            @click="getTodos(page)"
+            >{{ page }}</a
+          >
+        </li>
+        <li v-if="numberOfPages !== currentPage" class="page-item">
+          <a
+            style="cursor: pointer"
+            class="page-link"
+            @click="getTodos(currentPage + 1)"
+            >Next</a
+          >
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
@@ -35,16 +71,28 @@ export default {
   setup() {
     const todos = ref([])
     const error = ref('')
+    const numberOfTodos = ref(0)
+    const limit = 5
+    const currentPage = ref(1)
 
-    const getTodos = async () => {
+    const numberOfPages = computed(() => {
+      return Math.ceil(numberOfTodos.value / limit)
+    })
+
+    const getTodos = async (page = currentPage.value) => {
+      currentPage.value = page
       try {
-        const res = await axios.get('http://localhost:3000/todos')
+        const res = await axios.get(
+          `http://localhost:3000/todos?_page=${page}&_limit=${limit}`
+        )
+        numberOfTodos.value = res.headers['x-total-count']
         todos.value = res.data
       } catch (err) {
         console.log(err)
         error.value = 'Wrong'
       }
     }
+
     getTodos()
 
     const addTodo = async (todo) => {
@@ -65,6 +113,7 @@ export default {
       const id = todos.value[index].id
       try {
         await axios.delete('http://localhost:3000/todos/' + id)
+
         todos.value.splice(index, 1)
       } catch (err) {
         console.log(err)
@@ -72,8 +121,18 @@ export default {
       }
     }
 
-    const toggleTodo = (index) => {
-      todos.value[index].completed = !todos.value[index].completed
+    const toggleTodo = async (index) => {
+      error.value = ''
+      const id = todos.value[index].id
+      try {
+        await axios.patch('http://localhost:3000/todos/' + id, {
+          completed: !todos.value[index].completed
+        })
+        todos.value[index].completed = !todos.value[index].completed
+      } catch (err) {
+        console.log(err)
+        error.value = 'Wrong'
+      }
     }
 
     const searchText = ref('')
@@ -92,7 +151,10 @@ export default {
       toggleTodo,
       searchText,
       filteredTodos,
-      error
+      error,
+      numberOfPages,
+      currentPage,
+      getTodos
     }
   }
 }
